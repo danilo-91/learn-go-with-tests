262e33ca1715ce1646926709389552feab9d10c9
Daniel Ise <ise.daniel@gmail.com>
2023-09-25 20:32:32 -0300

    roman to arabic implemented (cheating because I'm lazy)

--- intro-to-property-based-tests/roman.go
+++ intro-to-property-based-tests/roman.go
@@ -1,29 +1,45 @@
 package roman
 
 import (
-    "strings"
+	"strings"
 )
 
 type RomanNumeral struct {
-    arabic int
-    roman string
+	arabic int
+	roman  string
 }
 
-var RomanNumerals = []RomanNumeral {
-    {10, "X"},
-    {9, "IX"},
-    {5, "V"},
-    {4, "IV"},
-    {1, "I"},
+var RomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
 }
 
 func AToRoman(arabic int) string {
-    var roman strings.Builder
-    for _, numeral := range RomanNumerals {
-        for arabic >= numeral.arabic {
-            roman.WriteString(numeral.roman)
-            arabic -= numeral.arabic
-        }
-    }
+	var roman strings.Builder
+	for _, numeral := range RomanNumerals {
+		for arabic >= numeral.arabic {
+			roman.WriteString(numeral.roman)
+			arabic -= numeral.arabic
+		}
+	}
 	return roman.String()
+}
+
+func RToArabic(roman string) int {
+	arabic := 1
+	for r := AToRoman(arabic); !(roman == r); r = AToRoman(arabic) {
+		arabic++
+	}
+	return arabic
 }
--- intro-to-property-based-tests/roman_test.go
+++ intro-to-property-based-tests/roman_test.go
@@ -1,52 +1,83 @@
 package roman
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestAToRoman(t *testing.T) {
-    cases := []struct {
-        Description string
-        n int
-        roman string
-    }{
-        {"1 to I", 1, "I"},
-        {"2 to II", 2, "II"},
-        {"4 to IV", 4, "IV"},
-        {"5 to V", 5, "V"},
-        {"6 to VI", 6, "VI"},
-        {"7 to VII", 7, "VII"},
-        {"8 to VIII", 8, "VIII"},
-        {"9 to IX", 9, "IX"},
-        {"10 to X", 10, "X"},
-        {"11 to XI", 11, "XI"},
-        {"12 to XII", 12, "XII"},
-        {"13 to XIII", 13, "XIII"},
-        {"14 to XIV", 14, "XIV"},
-        {"15 to XV", 15, "XV"},
-        {"16 to XVI", 16, "XVI"},
-        {"17 to XVII", 17, "XVII"},
-        {"18 to XVIII", 18, "XVIII"},
-        {"19 to XIX", 19, "XIX"},
-        {"20 to XX", 20, "XX"},
-        {"25 to XXV", 25, "XXV"},
-        {"30 to XXX", 30, "XXX"},
-        {"36 to XXXVI", 36, "XXXVI"},
-    }
+var cases = []struct {
+	arabic int
+	roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{11, "XI"},
+	{12, "XII"},
+	{13, "XIII"},
+	{14, "XIV"},
+	{15, "XV"},
+	{16, "XVI"},
+	{17, "XVII"},
+	{18, "XVIII"},
+	{19, "XIX"},
+	{20, "XX"},
+	{25, "XXV"},
+	{30, "XXX"},
+	{36, "XXXVI"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{100, "C"},
+	{90, "XC"},
+	{400, "CD"},
+	{500, "D"},
+	{900, "CM"},
+	{1000, "M"},
+	{1984, "MCMLXXXIV"},
+	{3999, "MMMCMXCIX"},
+	{2014, "MMXIV"},
+	{1006, "MVI"},
+	{798, "DCCXCVIII"},
+}
 
-    for _, test := range cases {
-        t.Run(test.Description, func(t *testing.T) {
-            got := AToRoman(test.n)
-            want := test.roman
-            assertString(t, got, want)
-        })
-    }
+func TestAToRoman(t *testing.T) {
+	for _, test := range cases {
+		t.Run(test.roman, func(t *testing.T) {
+			got := AToRoman(test.arabic)
+			want := test.roman
+			assertString(t, got, want)
+		})
+	}
+}
+
+func TestRToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q to %d", test.roman, test.arabic), func(t *testing.T) {
+			got := RToArabic(test.roman)
+			want := test.arabic
+			assertInt(t, got, want)
+		})
+	}
 }
 
 func assertString(t testing.TB, got, want string) {
-    t.Helper()
-    if got != want {
-        t.Errorf("got %q, but wanted %q", got, want)
-    }
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, but wanted %q", got, want)
+	}
 }
 
+func assertInt(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d but wanted %d", got, want)
+	}
+}
