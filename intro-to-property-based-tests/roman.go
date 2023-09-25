c01150a6a4833be24fd8a8fb3efe27da19d4027d
Daniel Ise <ise.daniel@gmail.com>
2023-09-25 19:06:19 -0300

    implemented to 39, changed switch to slice of struct

--- intro-to-property-based-tests/roman.go
+++ intro-to-property-based-tests/roman.go
@@ -1,11 +1,29 @@
 package roman
 
-func NToRoman(n int) string {
-	roman := ""
-	for n > 0 {
-		roman += "I"
-		n -= 1
-	}
-	return roman
+import (
+    "strings"
+)
 
+type RomanNumeral struct {
+    arabic int
+    roman string
+}
+
+var RomanNumerals = []RomanNumeral {
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"},
+}
+
+func AToRoman(arabic int) string {
+    var roman strings.Builder
+    for _, numeral := range RomanNumerals {
+        for arabic >= numeral.arabic {
+            roman.WriteString(numeral.roman)
+            arabic -= numeral.arabic
+        }
+    }
+	return roman.String()
 }
--- intro-to-property-based-tests/roman_test.go
+++ intro-to-property-based-tests/roman_test.go
@@ -4,24 +4,43 @@ import (
 	"testing"
 )
 
-func TestNToRoman(t *testing.T) {
-    t.Run("I", func(t *testing.T) {
-        got := NToRoman(1)
-        want := "I"
-        assertString(t, got, want)
-    })
+func TestAToRoman(t *testing.T) {
+    cases := []struct {
+        Description string
+        n int
+        roman string
+    }{
+        {"1 to I", 1, "I"},
+        {"2 to II", 2, "II"},
+        {"4 to IV", 4, "IV"},
+        {"5 to V", 5, "V"},
+        {"6 to VI", 6, "VI"},
+        {"7 to VII", 7, "VII"},
+        {"8 to VIII", 8, "VIII"},
+        {"9 to IX", 9, "IX"},
+        {"10 to X", 10, "X"},
+        {"11 to XI", 11, "XI"},
+        {"12 to XII", 12, "XII"},
+        {"13 to XIII", 13, "XIII"},
+        {"14 to XIV", 14, "XIV"},
+        {"15 to XV", 15, "XV"},
+        {"16 to XVI", 16, "XVI"},
+        {"17 to XVII", 17, "XVII"},
+        {"18 to XVIII", 18, "XVIII"},
+        {"19 to XIX", 19, "XIX"},
+        {"20 to XX", 20, "XX"},
+        {"25 to XXV", 25, "XXV"},
+        {"30 to XXX", 30, "XXX"},
+        {"36 to XXXVI", 36, "XXXVI"},
+    }
 
-    t.Run("III", func(t *testing.T) {
-        got := NToRoman(3)
-        want := "III"
-        assertString(t, got, want)
-    })
-
-    t.Run("IV", func(t *testing.T) {
-        got := NToRoman(4)
-        want := "IV"
-        assertString(t, got, want)
-    })
+    for _, test := range cases {
+        t.Run(test.Description, func(t *testing.T) {
+            got := AToRoman(test.n)
+            want := test.roman
+            assertString(t, got, want)
+        })
+    }
 }
 
 func assertString(t testing.TB, got, want string) {
