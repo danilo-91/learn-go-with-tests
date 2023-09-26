ab2b18745b0a8eb5ac2da607c859d56d41338306
Daniel Ise <ise.daniel@gmail.com>
2023-09-26 19:05:21 -0300

    implemented sec to radiansss

--- maths/clock.go
+++ maths/clock.go
@@ -1,14 +1,21 @@
 package clock
 
-import "time"
-
-// Returns the second point of the hour hand of an analogue clock
-func SecondHand(t time.Time) Point {
-	return Point{}
-}
+import (
+	"math"
+	"time"
+)
 
 // Represents a two dimensional coordinate
 type Point struct {
 	X float64
 	Y float64
+}
+
+// Returns the second point of the hour hand of an analogue clock
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func SecToRadian(t time.Time) float64 {
+	return math.Pi
 }
--- maths/clock_test.go
+++ maths/clock_test.go
@@ -1,17 +1,44 @@
 package clock
 
 import (
+	"math"
 	"testing"
 	"time"
 )
 
 func TestSecondHand(t *testing.T) {
-	d := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	t.Run("midnight", func(t *testing.T) {
+		hour := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+		want := Point{X: 150, Y: 150 - 90}
+		got := SecondHand(hour)
+		assertPoints(t, got, want)
+	})
 
-	want := Point{X: 150, Y: 150 - 90}
-	got := SecondHand(d)
+	t.Run("6 o'clock", func(t *testing.T) {
+		hour := time.Date(1337, time.January, 1, 6, 0, 0, 0, time.UTC)
+		want := Point{X: 150, Y: 150 + 90}
+		got := SecondHand(hour)
+		assertPoints(t, got, want)
+	})
+}
 
+func TestSecToRadian(t *testing.T) {
+	thirtySec := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := SecToRadian(thirtySec)
+	assertFloat64(t, got, want)
+}
+
+func assertPoints(t testing.TB, got, want Point) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
+
+func assertFloat64(t testing.TB, got, want float64) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, but wanted %v", got, want)
 	}
 }
