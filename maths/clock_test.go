fb77328004f9b9275c4e1bdacc8bc005db0ea2e0
Daniel Ise <ise.daniel@gmail.com>
2023-09-26 19:18:02 -0300

    created test for sec to radss

--- maths/clock_test.go
+++ maths/clock_test.go
@@ -23,10 +23,22 @@ func TestSecondHand(t *testing.T) {
 }
 
 func TestSecToRadian(t *testing.T) {
-	thirtySec := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-	want := math.Pi
-	got := SecToRadian(thirtySec)
-	assertFloat64(t, got, want)
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
+	for _, c := range cases {
+		t.Run(timeName(c.time), func(t *testing.T) {
+			want := c.angle
+			got := SecToRadian(c.time)
+			assertFloat64(t, got, want)
+		})
+	}
 }
 
 func assertPoints(t testing.TB, got, want Point) {
@@ -41,4 +53,12 @@ func assertFloat64(t testing.TB, got, want float64) {
 	if got != want {
 		t.Errorf("got %v, but wanted %v", got, want)
 	}
+}
+
+func simpleTime(hour, minutes, seconds int) time.Time {
+    return time.Date(1337, time.January, 1, hour, minutes, seconds, 0, time.UTC)
+}
+
+func timeName(t time.Time) string {
+    return t.Format("15:04:05")
 }
