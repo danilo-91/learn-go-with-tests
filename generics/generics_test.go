e63479596fac878b4d9e77e9e1fed885ab6e1038
Daniel Ise <ise.daniel@gmail.com>
2023-10-10 19:32:48 -0300

    test cleanup for final version

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -2,23 +2,8 @@ package generics_test
 
 import (
 	"testing"
-
 	"github.com/isedaniel/generics"
 )
-
-// func TestAssertFunction(t *testing.T) {
-//     t.Run("asserting integers", func(t *testing.T) {
-//         AssertEqual(t, 1, 1)
-//         AssertNotEqual(t, 1, 2)
-//     })
-//
-//     t.Run("asserting strings", func(t *testing.T) {
-//         AssertEqual(t, "a string", "a string")
-//         AssertNotEqual(t, "a string", "not a string")
-//     })
-//
-//     //AssertEqual(t, 1, "1")
-// }
 
 func TestStack(t *testing.T) {
     t.Run("integer stack", func(t *testing.T) {
@@ -45,25 +30,6 @@ func TestStack(t *testing.T) {
         first, _ := s.Pop()
         second, _ := s.Pop()
         AssertEqual(t, first + second, 3)
-    })
-
-    t.Run("string stack", func(t *testing.T){
-        s := new(generics.Stack[string])
-
-        // check empty stack
-        AssertTrue(t, s.IsEmpty())
-
-        // add string, check not empty
-        s.Push("1")
-        AssertFalse(t, s.IsEmpty())
-
-        // add another, pop back, check empty
-        s.Push("2")
-        v, _ := s.Pop()
-        AssertEqual(t, v, "2")
-        v, _ = s.Pop()
-        AssertEqual(t, v, "1")
-        AssertTrue(t, s.IsEmpty())
     })
 }
 
