f428669fb9ca86a16b1639db622623f2790e185b
Daniel Ise <ise.daniel@gmail.com>
2023-10-11 17:56:31 -0300

    go fmt

--- revisiting-arrays-and-slices/arrays.go
+++ revisiting-arrays-and-slices/arrays.go
@@ -1,27 +1,26 @@
 package main
 
 func Sum(nums []int) int {
-    add := func(acc, el int) int { return acc + el }
-    return Reduce[int](nums, add, 0)
+	add := func(acc, el int) int { return acc + el }
+	return Reduce[int](nums, add, 0)
 }
 
 func SumAllTails(slices ...[]int) []int {
-    addTails := func(acc, el []int) []int { 
-        if len(el) == 0 {
-            return append(acc, 0)
-        } else {
-            return append(acc, Sum(el[1:])) 
-        }
-    }
-    return Reduce[[]int](slices, addTails, []int{})
+	addTails := func(acc, el []int) []int {
+		if len(el) == 0 {
+			return append(acc, 0)
+		} else {
+			return append(acc, Sum(el[1:]))
+		}
+	}
+	return Reduce[[]int](slices, addTails, []int{})
 
 }
 
 func Reduce[A any](collection []A, acc func(A, A) A, initialValue A) A {
-    var result = initialValue
-    for _, v := range collection {
-        result = acc(result, v)
-    }
-    return result
+	var result = initialValue
+	for _, el := range collection {
+		result = acc(result, el)
+	}
+	return result
 }
-
