76b8045d9c8c2fb21de4b5df932f9196c1cb5251
Daniel <ise.daniel@gmail.com>
2023-10-18 18:55:52 -0300

    test re-written

--- scaling-acceptance-tests/cmd/httpserver/greeter_server_test.go
+++ scaling-acceptance-tests/cmd/httpserver/greeter_server_test.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"context"
+	"github.com/alecthomas/assert/v2"
 	go_specs_greet "github.com/isedaniel/go-specs-greet"
 	"github.com/isedaniel/go-specs-greet/specifications"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 	"testing"
 )
 
 func TestGreeterServer(t *testing.T) {
+	ctx := context.Background()
+
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context: "../../.",
+			Dockerfile: "./cmd/httpserver/Dockerfile",
+			PrintBuildLog: true,
+		},
+		ExposedPorts: []string{"8080:8080"},	
+		WaitingFor: wait.ForHTTP("/").WithPort("8080"),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started: true,
+		})
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		assert.NoError(t, container.Terminate(ctx))
+	})
+
 	driver := go_specs_greet.Driver{BaseURL: "http://localhost:8080"}
 	specifications.GreetSpecifications(t, driver)
 }
