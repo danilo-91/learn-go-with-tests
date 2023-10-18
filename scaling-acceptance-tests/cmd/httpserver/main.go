e3dbcec68bd963a141352f583041a0337c8c86cb
Daniel <ise.daniel@gmail.com>
2023-10-18 20:36:40 -0300

    added handler.go and cleaned from main.go

--- scaling-acceptance-tests/cmd/httpserver/main.go
+++ scaling-acceptance-tests/cmd/httpserver/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"github.com/isedaniel/go-specs-greet"
 )
 
 func main() {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, World")
-	})
+	handler := http.HandlerFunc(go_specs_greet.Handler)
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
