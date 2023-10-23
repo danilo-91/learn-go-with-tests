98077f332e3c10975fb76d3d35ba00f077ba36a2
Daniel <ise.daniel@gmail.com>
2023-10-23 19:15:47 -0300

    first functional main.go

--- app-one/main.go
+++ app-one/main.go
@@ -5,8 +5,14 @@ import (
 	"net/http"
 )
 
+type MemoryStorage struct {}
+
+func (m *MemoryStorage) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&MemoryStorage{}}
 	handler := http.HandlerFunc(server.ServeHTTP)
 	log.Fatal(http.ListenAndServe(":5000", handler))
 }
